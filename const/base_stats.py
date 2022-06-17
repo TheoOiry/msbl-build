977f4029f5e31945d9c1fb84dc7902bc67ef8661
TheoOiry <theo.oiry@yahoo.fr>
2022-06-18 01:39:46 +0200

    Fix bad stats

--- const/base_stats.py
+++ const/base_stats.py
@@ -2,7 +2,7 @@ BASE_STATS = {
     "mario": [11, 12, 14, 10, 16],
     "luigi": [11, 11, 10, 14, 17],
     "bowser": [17, 9, 17, 11, 9],
-    "peach": [9, 17, 9, 10, 13],
+    "peach": [9, 17, 9, 13, 15],
     "rosalina": [14, 9, 17, 10, 13],
     "toad": [9, 17, 11, 15, 11],
     "yoshi": [10, 10, 17, 17, 9],
@@ -10,3 +10,5 @@ BASE_STATS = {
     "wario": [17, 9, 15, 13, 9],
     "waluigi": [15, 16, 9, 9, 14]
 }
+
+STATS_NAMES = ["strength", "speed", "shoot", "pass", "technic"]
